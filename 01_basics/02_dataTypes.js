39050389a7b0840938a62b4f1eecb4c11a47f5b4
rupam9876 <145781613+rupam9876@users.noreply.github.com>
2023-10-02 14:56:33 +0000

    data Type done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all jd code as newer version
+
+//alert(3+3)// we are using nodejs,not using browser
+
+console.log(3
+    +3)// code readibality should be high
+
+console.log("Rupam")
+
+
+
+let name ="Rupam"
+let age ="18"
+let isLoggedIn = false
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol => unique
+
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
